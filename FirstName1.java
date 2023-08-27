015e27d8eedbc6c7f40a597eb87122d1412666b3
Denis <143257134+PirogoffDenis@users.noreply.github.com>
2023-08-28 00:05:14 +0300

    Add files via upload

--- FirstName1.java
+++ FirstName1.java
@@ -1,19 +1,34 @@
 public class FirstName1 {
     public static void main(String[] args) {
 
-        int[] massiv = new int[8];
+        int[] mas = new int[8];
 
-        for (int i = massiv.length; i > -1; i--) {
+        for (int x = mas.length; x > -1; x--) {
 
-            System.out.println("От большего к меньшему = " + i);
+            System.out.println("От большего к меньшему = " + x);
         }
         System.out.println("   ");
-        int[] massiv1 = {
+        int[] mas1 = {
                 4, 6, 7, 5, 4, 3, 7, 7, 5, 4};
 
-        for (int i = 0; i < massiv1.length; i++) {
+        for (int s = 0; s < mas1.length; s++) {
 
-            System.out.println("От меньшего к большему = " + i);
+            System.out.println("От меньшего к большему = " + s);
         }
+        System.out.println("   ");
+
+        int[] sum = new int[mas.length + mas1.length];
+        int e = 0;
+
+        for (int i = 0; i < mas.length; i++) {
+            sum[i] = mas[i];
+            e++;
+        }
+        for (int j = 0; j < mas1.length; j++) {
+            sum[e++] = mas1[j];
+        }
+        for (int i = 0; i < sum.length; i++)
+
+            System.out.print("+" + sum[i]);
     }
 }
